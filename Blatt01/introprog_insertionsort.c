ee5bc6996fce54ffe5feeb0d2fa07407420c3982
Jannik <34013062+Ashiven@users.noreply.github.com>
2018-01-18 16:29:38 +0100

    Update introprog_insertionsort.c

--- Blatt01/introprog_insertionsort.c
+++ Blatt01/introprog_insertionsort.c
@@ -1,7 +1,7 @@
 /* === INTROPROG ABGABE ===
  * Blatt 1, Aufgabe 1
- * Tutorium: t24
- * Abgabe von: Jannik Novak
+ * Tutorium: txx
+ * Abgabe von: Max Mustermann
  * ========================
  */
 
