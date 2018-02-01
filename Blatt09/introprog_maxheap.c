1ae9a634ce3a13ebd353f4779a88a6244dc1607b
Ashiven <34013062+Ashiven@users.noreply.github.com>
2018-02-01 22:04:56 +0100

    Update introprog_maxheap.c

--- Blatt09/introprog_maxheap.c
+++ Blatt09/introprog_maxheap.c
@@ -1,10 +1,10 @@
 /* === INTROPROG ABGABE ===
  * Blatt 9, Aufgabe 2
- * Tutorium: t21
- * Gruppe: g07
+ * Tutorium: txx
+ * Gruppe: gxx
  * Gruppenmitglieder:
- *  - Ashkan Ardeshirdavani
- *  - Jannik Novak
+ *  - Max Mustermann
+ *  - Rainer Testfall
  * ========================
  */
 
